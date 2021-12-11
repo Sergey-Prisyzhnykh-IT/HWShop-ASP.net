a5cbf94dfecf770b4ce0dc2fc51e8d63fd7402a1
Sergey <joker3839@gmail.com>
2021-12-11 20:46:47 +0300

    Накостылил караул
    По нормальному надо все переписать

--- HWShop-ASP.net/Product/Basket.cs
+++ HWShop-ASP.net/Product/Basket.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections.Concurrent;
 using System.Text;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace HWShop_ASP.net.Product
 {
     public class Basket : IBasket
     {
-        ProtectedLocalStorage _localStorage;
         public ConcurrentDictionary<HW2OnlineShop.Product, int> _products = new ConcurrentDictionary<HW2OnlineShop.Product, int>();
+        public static ConcurrentDictionary<string, int> lastProducts = new ConcurrentDictionary<string, int>();
 
         public void Buy(HW2OnlineShop.Product product, int count)
         {
@@ -18,7 +17,6 @@ namespace HWShop_ASP.net.Product
         {
             return _products;
         }
-
         public void Clear()
         {
             _products.Clear();
@@ -53,6 +51,16 @@ namespace HWShop_ASP.net.Product
                     tempCountProduct--;
                     _products.TryUpdate(product.Key, tempCountProduct, product.Value);
                 }
+        }
+
+        public void AddLaastShop(string name, int count)
+        {
+            lastProducts.TryAdd(name, count);
+        }
+
+        public ConcurrentDictionary<string, int> GetLastProduct() 
+        {
+            return lastProducts;
         }
     }
 }
--- HWShop-ASP.net/Product/IBasket.cs
+++ HWShop-ASP.net/Product/IBasket.cs
@@ -6,6 +6,8 @@ namespace HWShop_ASP.net.Product
     public interface IBasket
     {
         void Buy(HW2OnlineShop.Product product, int count);
+        void AddLaastShop(string name, int count);
+        ConcurrentDictionary<string, int> GetLastProduct();
         ConcurrentDictionary<HW2OnlineShop.Product, int> ShowBusket();
         void Clear();
         public int GetProductCount(string productName);
