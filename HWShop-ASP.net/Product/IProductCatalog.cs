d20d5ec33bcb0048564bb375b0b9510ff984c255
Sergey <joker3839@gmail.com>
2021-12-18 15:57:04 +0300

    Доделал Практику

--- HWShop-ASP.net/Product/IProductCatalog.cs
+++ HWShop-ASP.net/Product/IProductCatalog.cs
@@ -3,8 +3,8 @@ namespace HW2OnlineShop
 {
     public interface IProductCatalog
     {
+        void AddDBProduct(Product products);
+        Product GetProduct(string productName);
         IReadOnlyCollection<Product> GetProductCatalog();
-        public Product GetProduct(string productName);
-
     }
 }
